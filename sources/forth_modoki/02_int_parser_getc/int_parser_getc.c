37b15ee439874cd0bfa3bfc0538d244964c07a68
Kazuki Shigemichi <shigemichik@gmail.com>
2022-05-17 19:31:49 +0900

    add test

--- sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
+++ sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
@@ -81,12 +81,30 @@ void test_parse_one_123_456(void) {
   assert(token_type == TOKEN_TYPE_NUMBER);
 }
 
-int main() {
-  int answer1 = 0;
-  int answer2 = 0;
+void test_parse_one_123__456(void) {
+  cl_getc_set_src("123  456  ");
 
+  int answer = 0;
+  int c = '\0';
+  token_type_t token_type;
+
+  c = parse_one(c, &answer, &token_type);
+  assert(answer == 123);
+  assert(token_type == TOKEN_TYPE_NUMBER);
+
+  c = parse_one(c, &answer, &token_type);
+  assert(answer == 0);
+  assert(token_type == TOKEN_TYPE_SPACE);
+
+  c = parse_one(c, &answer, &token_type);
+  assert(answer == 456);
+  assert(token_type == TOKEN_TYPE_NUMBER);
+}
+
+int main() {
   test_parse_one_123();
   test_parse_one_123_456();
+  test_parse_one_123__456();
 
   return 0;
 }
